979adec6577d240b8ee5ffa036cd51c400b96ff4
Froloket64 <ket.frol2006@gmail.com>
2024-05-27 18:44:06 +0300

    Update benchmarks
    - Edited old ones a bit
    - Added randomized ones

--- benches/bench.rs
+++ benches/bench.rs
@@ -1,8 +1,10 @@
 use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion};
 use itertools::Itertools;
 use peeking_iter::PeekingIter;
+use rand::random;
+use std::iter;
 
-fn peeking(c: &mut Criterion) {
+fn peek(c: &mut Criterion) {
     let mut group = c.benchmark_group("Peeking");
     let mut multipeek = (0..1000).multipeek();
     let mut peeking_iter = PeekingIter::new(0..1000);
@@ -20,31 +22,26 @@ fn peeking(c: &mut Criterion) {
     });
 }
 
-fn next_peek(c: &mut Criterion) {
-    let mut group = c.benchmark_group("next() + peek()");
-    let mut multipeek = (0..1000).multipeek();
-    let mut peeking_iter = PeekingIter::new(0..1000);
+fn peek_random(c: &mut Criterion) {
+    let mut group = c.benchmark_group("Peeking");
+    let it = iter::from_fn(|| Some(random::<u32>())).cycle();
+    let mut multipeek = it.clone().multipeek();
+    let mut peeking_iter = PeekingIter::new(it);
 
-    group.bench_function(BenchmarkId::new("itertools:MultiPeek", "(0..1000)"), |b| {
+    group.bench_function(BenchmarkId::new("itertools::MultiPeek", "random"), |b| {
         b.iter(|| {
             multipeek.next();
-            multipeek.peek();
-            multipeek.peek();
-            multipeek.next();
-        });
+        })
     });
 
-    group.bench_function(BenchmarkId::new("PeekingIter", "(0..1000)"), |b| {
+    group.bench_function(BenchmarkId::new("PeekingIter", "random"), |b| {
         b.iter(|| {
             peeking_iter.next();
-            peeking_iter.peek();
-            peeking_iter.peek();
-            peeking_iter.next();
-        });
+        })
     });
 }
 
-fn plain_next(c: &mut Criterion) {
+fn next(c: &mut Criterion) {
     let mut group = c.benchmark_group("Plain next()");
     let mut multipeek = (0..1000).multipeek();
     let mut peeking_iter = PeekingIter::new(0..1000);
@@ -69,5 +66,90 @@ fn plain_next(c: &mut Criterion) {
     });
 }
 
-criterion_group! { benches, peeking, next_peek, plain_next }
-criterion_main! { benches }
+fn next_random(c: &mut Criterion) {
+    let mut group = c.benchmark_group("Plain next()");
+    let mut it = iter::from_fn(|| Some(random::<u32>())).cycle();
+    let mut multipeek = it.clone().multipeek();
+    let mut peeking_iter = PeekingIter::new(it.clone());
+
+    group.bench_function(BenchmarkId::new("std::ops::Range", "random"), |b| {
+        b.iter(|| {
+            it.next();
+        })
+    });
+
+    group.bench_function(BenchmarkId::new("itertools::MultiPeek", "random"), |b| {
+        b.iter(|| {
+            multipeek.next();
+        })
+    });
+
+    group.bench_function(BenchmarkId::new("PeekingIter", "random"), |b| {
+        b.iter(|| {
+            peeking_iter.next();
+        })
+    });
+}
+
+fn next_peek(c: &mut Criterion) {
+    let mut group = c.benchmark_group("next() + peek()");
+    let mut multipeek = (0..1000).multipeek();
+    let mut peeking_iter = PeekingIter::new(0..1000);
+
+    group.bench_function(BenchmarkId::new("itertools::MultiPeek", "(0..1000)"), |b| {
+        b.iter(|| {
+            multipeek.next();
+            multipeek.peek();
+            multipeek.peek();
+            multipeek.next();
+        });
+    });
+
+    group.bench_function(BenchmarkId::new("PeekingIter", "(0..1000)"), |b| {
+        b.iter(|| {
+            peeking_iter.next();
+            peeking_iter.peek();
+            peeking_iter.peek();
+            peeking_iter.next();
+        });
+    });
+}
+
+fn next_peek_random(c: &mut Criterion) {
+    let mut group = c.benchmark_group("next() + peek()");
+    let it = iter::from_fn(|| Some(random::<u32>())).cycle();
+    let mut multipeek = it.clone().multipeek();
+    let mut peeking_iter = PeekingIter::new(it);
+
+    group.bench_function(BenchmarkId::new("itertools::MultiPeek", "random"), |b| {
+        b.iter(|| {
+            multipeek.next();
+            multipeek.peek();
+            multipeek.peek();
+            multipeek.next();
+        });
+    });
+
+    group.bench_function(BenchmarkId::new("PeekingIter", "random"), |b| {
+        b.iter(|| {
+            peeking_iter.next();
+            peeking_iter.peek();
+            peeking_iter.peek();
+            peeking_iter.next();
+        });
+    });
+}
+
+fn next_while(c: &mut Criterion) {
+    let it = iter::from_fn(|| Some(random::<u32>())).cycle();
+    let mut peeking_iter = PeekingIter::new(it);
+
+    c.bench_function("next_while", |b| {
+        b.iter(|| peeking_iter.next());
+    });
+}
+
+criterion_group! { compare_preset, peek, next, next_peek }
+criterion_group! { compare_random, peek_random, next_random, next_peek_random }
+criterion_group! { targeted, next_while }
+criterion_main! { compare_preset, compare_random }
