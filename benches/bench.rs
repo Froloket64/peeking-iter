1ecd891594f3fe45c78364b2e96e9dbdb32bfae4
Froloket64 <ket.frol2006@gmail.com>
2025-08-08 18:35:48 +0300

    fix(bench): Fix import path

--- benches/bench.rs
+++ benches/bench.rs
@@ -1,6 +1,6 @@
 use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion};
 use itertools::Itertools;
-use peeking_iter::PeekingIter;
+use peeking_iter::peeking::*;
 use rand::random;
 use std::iter;
 
